48da82353a12f0e73a7ab50673f087ada8cb8d3a
Ankush-Agnihotri <ankushas2001@gmail.com>
2024-10-18 11:33:39 +0530

    Update main.js to restart the working of jenkins

--- main.js
+++ main.js
@@ -49,8 +49,4 @@ app.use("/api/staff", libraryStaffRouter);
 app.listen(PORT, (req, res) => {
   console.log(`The server is running at port http://localhost:${PORT}`);
 });
-//this comment is added to check the jenkins working
-//this comment is to test that the jenkins is running automatically and manually
-//another testing of jenkins pipeline
-//Testing of the jenkins script
-//Repairing the cloud burst again
+//this comment is added to check the jenkins working with the github or not
